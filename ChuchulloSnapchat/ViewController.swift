dc95db1c9c0588cba35e78f579515bbd7873b3a4
Roy Shaider Chuchullo Pucho <roy.chuchullo@tecsup.edu.pe>
2024-10-14 16:42:37 -0500

    Initial Commit

---
+++ ChuchulloSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChuchulloSnapchat
+//
+//  Created by Roy Shaider Chuchullo Pucho on 14/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
